57777441de8a0557c09b14de3bbfea34248a4c92
Jun Sekine <jun@sakamo.to>
2024-06-21 13:11:08 +0900

    publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     val kotlinVersion = "1.9.20"
     kotlin("multiplatform") version kotlinVersion
     id("org.jetbrains.kotlinx.kover") version "0.7.5"
     id("org.jetbrains.dokka") version "1.9.10"
     id("com.android.library") version "8.2.0"
-    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 group = "com.jsoizo"
@@ -17,9 +19,6 @@ repositories {
 
 kotlin {
     applyDefaultHierarchyTemplate()
-    jvm {
-        withSourcesJar()
-    }
 
     sourceSets {
         val commonMain by getting {
@@ -34,8 +33,11 @@ kotlin {
         }
     }
 
+    jvm {
+        withSourcesJar(true)
+    }
     androidTarget {
-        publishLibraryVariants("release")
+        publishLibraryVariants("release", "debug")
         compilations.all {
             kotlinOptions {
                 jvmTarget = "1.8"
@@ -45,6 +47,7 @@ kotlin {
     iosArm64()
     iosSimulatorArm64()
     linuxX64()
+    macosArm64()
 }
 
 tasks.withType<Test>().configureEach {
@@ -59,5 +62,47 @@ java {
 
 android {
     namespace = "com.jsoizo.paritialfunction"
-    compileSdk = 35
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 28
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "partial-function-kt",
+        version = version.toString()
+    )
+
+    pom {
+        name.set("partial-function-kt")
+        description.set("Partial Function in Kotlin")
+        url.set("https://github.com/jsoizo/partial-function-kt")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/jsoizo/partial-function-kt/blob/master/LICENSE")
+                distribution.set("repo")
+
+            }
+        }
+        developers {
+            developer {
+                id.set("jsoizo")
+                name.set("Jun Sekine")
+                email.set("jun@sakamo.to")
+            }
+        }
+        scm {
+            url.set("https://github.com/jsoizo/partial-function-kt")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
